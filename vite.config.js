7072e1a278e9e9a6a368630b8d69aed8ceeb1f1f
mathieustrosberg <mathieustrosberg@gmail.com>
2024-05-11 19:16:12 +0200

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import { fileURLToPath, URL } from "url";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    __APP_ENV__: process.env.VITE_VERCEL_ENV,
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
